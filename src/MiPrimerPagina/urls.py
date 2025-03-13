f770a301b5ddbd9c3def1de456630af5a9c0685c
Mirabel <mirabelnaren@hotmail.com>
2025-03-13 17:48:52 -0300

    Terminamos la prueba de saludos

--- src/MiPrimerPagina/urls.py
+++ src/MiPrimerPagina/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludar/', views.saludar),
     path('saludar2/', views.saludar_con_etiqueta),
+    path('saludar3/<str:nombre>/<str:apellido>', views.saludar_con_parametros),
 ]
--- src/principal/views.py
+++ src/principal/views.py
@@ -7,4 +7,8 @@ def saludar(request):
 def saludar_con_etiqueta(request):
     return HttpResponse("<h1>Hola desde Django con etiquetas!</h1>")
 
-# Create your views here.
+def saludar_con_parametros(request, nombre: str, apellido: str):
+    nombre = nombre.capitalize()
+    apellido = apellido.capitalize()
+    return HttpResponse(f"Hola {nombre} {apellido} desde Django con parámetros!")
+# Va a ocurrir que cuando se abra el servidor va a dar un error porque faltan parametros. En el mismo link del servidor se agregan los valores para los parametros de la funcion. Por ejemplo http://127.0.0.1:8000/saludar3/Naren/Mirabel. Ademas hay que aclarar en la puerta de entrada, es decir en urls.py que debe tomar strings en el link
