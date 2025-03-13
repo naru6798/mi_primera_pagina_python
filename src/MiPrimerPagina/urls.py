11ad611d505c3ddc0b4c5097c38f5240ab665f79
Mirabel <mirabelnaren@hotmail.com>
2025-03-13 17:37:16 -0300

    Probando views

--- src/MiPrimerPagina/urls.py
+++ src/MiPrimerPagina/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from principal import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('saludar/', views.saludar),
+    path('saludar2/', views.saludar_con_etiqueta),
 ]
--- src/principal/views.py
+++ src/principal/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def saludar(request):
+    return HttpResponse("Hola desde Django!")
+
+def saludar_con_etiqueta(request):
+    return HttpResponse("<h1>Hola desde Django con etiquetas!</h1>")
 
 # Create your views here.
