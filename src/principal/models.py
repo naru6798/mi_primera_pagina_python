dcf1fc8f6f835fad8ad9c855df1906f4a0636bd7
Mirabel <mirabelnaren@hotmail.com>
2025-03-14 23:23:10 -0300

    Base de datos

--- src/principal/models.py
+++ src/principal/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
